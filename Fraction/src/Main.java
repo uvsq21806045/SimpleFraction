0169596cf72ec87957811c58d6f40b180c7cb5d3
Salim Slimani <salim.slimani@ens.uvsq.fr>
2020-10-04 15:50:35 +0200

    Modifying Main.java to test with Asserts.

--- Fraction/src/Main.java
+++ Fraction/src/Main.java
@@ -3,6 +3,6 @@ public class Main {
     public static void main(String[] args){
         Fraction frac = new Fraction(2, 1);
 
-        System.out.println("Votre fraction est: " + frac.toString());
+        assert frac.toString().equals("2/1"): "Error. 'Fraction' returns the wrong String.";
     }
 }
